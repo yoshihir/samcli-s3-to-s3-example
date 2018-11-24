30427da66f2da780bbe08c42713c724a84adc87a
yoshihiro.yoshida <>
2018-11-24 16:50:22 +0900

    s3 download処理作成

--- src/main.go
+++ src/main.go
@@ -4,21 +4,42 @@ import (
 	"context"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"io/ioutil"
+	"os"
 )
-
-// temp location to store image and thumbnail
-const tmp = "/tmp/"
-
-// S3 Session to use
-var sess = session.Must(session.NewSession())
-
-// Create a downloader with session and default option
-var downloader = s3manager.NewDownloader(sess)
 
 func handler(ctx context.Context, req events.S3Event) error {
 	return nil
+}
+
+func s3Download(bucket string, key string) (f *os.File, err error) {
+	var sess = session.Must(session.NewSession(&aws.Config{
+		S3ForcePathStyle: aws.Bool(true),
+		Region:           aws.String(os.Getenv("REGION")),
+		Endpoint:         aws.String(os.Getenv("S3_ENDPOINT")),
+	}))
+
+	tmpfile, _ := ioutil.TempFile("/tmp", "srctmp_")
+	defer os.Remove(tmpfile.Name())
+
+	// ダウンロード処理
+	var downloader = s3manager.NewDownloader(sess)
+
+	_, err = downloader.Download(
+		tmpfile,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(key),
+		})
+	if err != nil {
+		println("file download error")
+	}
+
+	return tmpfile, err
 }
 
 func main() {
--- src/main_test.go
+++ src/main_test.go
@@ -5,18 +5,71 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"io/ioutil"
 	"os"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
+	// test前処理
 	println("before all...")
 
+	os.Setenv("REGION", "ap-northeast-1")
+	os.Setenv("S3_ENDPOINT", "http://localhost:4572")
+
+	var sess = session.Must(session.NewSession(&aws.Config{
+		S3ForcePathStyle: aws.Bool(true),
+		Region:           aws.String(os.Getenv("REGION")),
+		Endpoint:         aws.String(os.Getenv("S3_ENDPOINT")),
+	}))
+	var creater = s3.New(sess)
+	var uploader = s3manager.NewUploader(sess)
+
+	input := &s3.CreateBucketInput{
+		Bucket: aws.String("bucket-example"),
+	}
+
+	_, err := creater.CreateBucket(input)
+	if err != nil {
+		//if aerr, ok := err.(awserr.Error); ok {
+		//	switch aerr.Code() {
+		//	case s3.ErrCodeBucketAlreadyExists:
+		//		fmt.Println(s3.ErrCodeBucketAlreadyExists, aerr.Error())
+		//	case s3.ErrCodeBucketAlreadyOwnedByYou:
+		//		fmt.Println(s3.ErrCodeBucketAlreadyOwnedByYou, aerr.Error())
+		//	default:
+		//		fmt.Println(aerr.Error())
+		//	}
+		//} else {
+		//	fmt.Println(err.Error())
+		//}
+		//return
+	}
+
+	up, err := os.Open("../example.json")
+	if err != nil {
+		fmt.Println("failed to open file")
+		return
+	}
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String("bucket-example"),
+		Key:    aws.String("example.json"),
+		Body:   up,
+	})
+	if err != nil {
+		fmt.Println("failed to upload file")
+		return
+	}
+
+	// test実行
 	code := m.Run()
-
+	// test後実行
 	println("after all...")
-
 	os.Exit(code)
 }
 
@@ -24,18 +77,26 @@ func TestHandler(t *testing.T) {
 	t.Run("handler input test", func(t *testing.T) {
 		raw, err := ioutil.ReadFile("../event_file.json")
 		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
+			t.Fatal("Error failed to event file load")
 		}
 		var event events.S3Event
 		json.Unmarshal(raw, &event)
 		err = handler(context.Background(), event)
 		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
+			t.Fatal("Error failed to s3 event")
 		}
 
 		println("Test Handler...")
+	})
+
+	t.Run("s3 download test", func(t *testing.T) {
+		tmpFile, err := s3Download("bucket-example", "example.json")
+		if err != nil {
+			t.Fatal("Error failed to s3 download")
+		}
+		if tmpFile.Name() == "" {
+			t.Errorf("got: %v\nwant: %v", "", "/tmp/srctmp_*********")
+		}
 	})
 
 	//
